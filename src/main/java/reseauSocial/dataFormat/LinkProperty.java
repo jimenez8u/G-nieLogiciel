0475571145fe7961674b5e755e50acd5375be229
ValentinTr <valentin54200@gmail.com>
2019-04-11 20:14:58 +0200

    Ajout de la fonction toString() à la classe LinkProperty

--- src/main/java/reseauSocial/dataFormat/LinkProperty.java
+++ src/main/java/reseauSocial/dataFormat/LinkProperty.java
@@ -3,27 +3,29 @@ package reseauSocial.dataFormat;
 public class LinkProperty {
 	private String propertyName;
 	private String propertyValue;
-	
-	public LinkProperty(String propertyName, String propertyValue) 
-	{
+
+	public LinkProperty(String propertyName, String propertyValue) {
 		this.propertyName = propertyName;
 		this.propertyValue = propertyValue;
 	}
 
-	public String getPropertyName() 
-	{
+	public String getPropertyName() {
 		return propertyName;
 	}
-	public void setPropertyName(String propertyName) 
-	{
+
+	public void setPropertyName(String propertyName) {
 		this.propertyName = propertyName;
 	}
-	public String getPropertyValue() 
-	{
+
+	public String getPropertyValue() {
 		return propertyValue;
 	}
-	public void setPropertyValue(String propertyValue) 
-	{
+
+	public void setPropertyValue(String propertyValue) {
 		this.propertyValue = propertyValue;
+	}
+
+	public String toString() {
+		return this.propertyName + ":" + this.propertyValue;
 	}
 }
