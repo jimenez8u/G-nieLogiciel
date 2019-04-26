c0d36132a7f0b30b83372fe8033875baa0eae608
ValentinTr <valentin54200@gmail.com>
2019-04-26 16:31:07 +0200

    Amélioration de la structure des composants swing

--- src/main/java/reseauSocial/dataFormat/Interface.java
+++ src/main/java/reseauSocial/dataFormat/Interface.java
@@ -12,6 +12,7 @@ import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.view.mxGraph;
@@ -20,45 +21,58 @@ public class Interface extends JFrame {
 
     /** Pour éviter un warning venant du JFrame */
     private static final long serialVersionUID = -8123406571694511514L;
-
-    public Interface() {
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        JButton bouton = new JButton("Insert file");
-        bouton.addActionListener(new ActionListener()
-        {
-          public void actionPerformed(ActionEvent e)
-          {
-        	  JFileChooser choix = new JFileChooser();
-        	  int retour=choix.showOpenDialog(panel);
-        	  if(retour==JFileChooser.APPROVE_OPTION){
-        	     // un fichier a été choisi (sortie par OK)
-        	     // nom du fichier  choisi 
-        	     choix.getSelectedFile().getName();
-        	     // chemin absolu du fichier choisi
-        	     choix.getSelectedFile().
-        	            getAbsolutePath();
-        	  }else;// pas de fichier choisi
-          }
-        });
-        panel.add(bouton);
-        getContentPane().add(BorderLayout.SOUTH, panel);
-		setVisible(true);
-        
-    }
-
+    
     /**
      * @param args
      */
     public static void main(String[] args) {
+         JPanel panelGraph = new JPanel();
+    	 JFrame frame = new JFrame();
+    	 frame.setVisible(true);
+    	 frame.setSize(800, 600);
+         JPanel panel = new JPanel();
+         JButton bouton = new JButton("Insert file");
+         bouton.addActionListener(new ActionListener()
+         {
+           public void actionPerformed(ActionEvent e)
+           {
+         	  JFileChooser choix = new JFileChooser();
+         	  int retour=choix.showOpenDialog(panel);
+         	  if(retour==JFileChooser.APPROVE_OPTION){
+         	     // un fichier a été choisi (sortie par OK)
+         	     // nom du fichier  choisi 
+         	     choix.getSelectedFile().getName();
+         	     // chemin absolu du fichier choisi
+         	     choix.getSelectedFile().
+         	            getAbsolutePath();
+         	     ArrayList<LinkProperty> propertyList = new ArrayList<LinkProperty>();
+         			ArrayList<Link> linkList = new ArrayList<Link>();
+         			
+         			LinkProperty lProperty = new LinkProperty("premier test", "oui");
+         			propertyList.add(lProperty);
+         			SocialNode sNJean = new SocialNode("Jean");
+         			Link sL = new Link("premier lien", propertyList, sNJean);
+         			linkList.add(sL);
+         			SocialNode sNJacques = new SocialNode("Jacques", linkList);
+         			
+         			SocialNetwork sN = new SocialNetwork();
+         			sN.addNode(sNJacques);
+         			sN.addNode(sNJean);
+         			panelGraph.setSize(400, 400);
+         			panelGraph.add(createGraph(sN.getSocialNetwork()));
+         			frame.invalidate();
+         			frame.validate();
+         			frame.repaint();
+         	  }else;// pas de fichier choisi
+           }
+         });
+         panel.add(bouton);
+         frame.getContentPane().add(BorderLayout.SOUTH, panel);
+         frame.getContentPane().add(BorderLayout.NORTH, panelGraph);
         
-        Interface frame = new Interface();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
-        frame.setVisible(true);
     }
     
-    public JComponent createGraph(ArrayList<SocialNode> reseau) {
+    public static JComponent createGraph(ArrayList<SocialNode> reseau) {
     	mxGraph graph = new mxGraph();
         Object parent = graph.getDefaultParent();
         HashMap<SocialNode, Object> noeuds = new HashMap<SocialNode, Object>();
@@ -83,7 +97,9 @@ public class Interface extends JFrame {
         }
         graph.getModel().endUpdate();
         mxGraphComponent graphComponent = new mxGraphComponent(graph);
-        graphComponent.setEnabled(false);
+        graphComponent.setSize(400, 400);
+        graphComponent.setEnabled(true);
+        graphComponent.setVisible(true);
         return graphComponent;
     }
 }
