2826c0887bfb70fb40d95368cc41759d73d97a3e
MartinLem <martinlemaitre@hotmail.fr>
2019-05-14 16:34:40 +0200

    generating equals and hashCode methods in class SocialNode

--- src/main/java/reseauSocial/dataFormat/SocialNode.java
+++ src/main/java/reseauSocial/dataFormat/SocialNode.java
@@ -42,4 +42,30 @@ public class SocialNode {
 	{
 		this.linkList.add(link);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SocialNode other = (SocialNode) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 }
