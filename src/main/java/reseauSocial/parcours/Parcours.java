5b55b3d6da8d8e0462cb76fc1c3a78b98af2af5c
Martin <martinlemaitre@hotmail.fr>
2019-05-17 18:13:18 +0200

    depth first search v1

--- src/main/java/reseauSocial/parcours/Parcours.java
+++ src/main/java/reseauSocial/parcours/Parcours.java
@@ -10,7 +10,7 @@ import reseauSocial.dataFormat.SocialNode;
 
 public class Parcours {
 	
-	
+	//parcours largeur d'abord
 	public List<SocialNode> parcoursLargeur(SocialNode noeud) {
 		List<SocialNode> noeudsVisites = new ArrayList<>();
 		Deque<SocialNode> noeudsSuivants = new LinkedList<>();
@@ -43,8 +43,48 @@ public class Parcours {
 		}
 		
 		return noeudsVisites;
+	}
+	
+	//parcours profonndeur d'abord
+	public List<SocialNode> parcoursProfondeur(SocialNode noeud) {
+		
+		List<SocialNode> noeudsVisites = new ArrayList<>();
+		return parcoursProfondeurRec(noeud,noeudsVisites);
 		
 	}
 	
+	private List<SocialNode> parcoursProfondeurRec(SocialNode noeud, List<SocialNode> noeudsVisites) {
+		//List<SocialNode> noeudsVisites = new ArrayList<>();
+		Deque<SocialNode> noeudsSuivants = new LinkedList<>();
+		
+		if(!noeudsVisites.contains(noeud)) {
+			noeudsVisites.add(noeud);
+			
+			for (Link lien : noeud.getLinkList()) {
+				if (lien.getNoeudDepart().equals(noeud)) {
+					noeudsSuivants.addLast(lien.getNoeudArrive());
+				}
+				else {
+					noeudsSuivants.add(lien.getNoeudDepart());
+				}
+			}
 
+			while(noeudsSuivants.peekFirst() != null) {
+				
+				noeudsVisites.addAll(parcoursProfondeurRec(noeudsSuivants.pollFirst(), noeudsVisites));
+			}
+			return noeudsVisites;
+		}
+		else {
+			return noeudsVisites;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
