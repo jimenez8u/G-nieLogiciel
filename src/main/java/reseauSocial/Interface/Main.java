168f5bbb889e390515e5198a57044316d5a69c8d
ValentinTr <valentin54200@gmail.com>
2019-05-21 09:56:47 +0200

    Refactoring class Interface

---
+++ src/main/java/reseauSocial/Interface/Main.java
@@ -0,0 +1,11 @@
+package reseauSocial.Interface;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Interface fenetre = new Interface();
+		
+	}
+
+}
