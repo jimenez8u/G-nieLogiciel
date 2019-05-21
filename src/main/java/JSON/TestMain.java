6a6cb3367bdb9a100ea279218cc039e1d2eeb7f0
loic <nier57@hotmail.fr>
2019-05-21 17:05:11 +0200

    Adding tests

--- src/main/java/JSON/TestMain.java
+++ src/main/java/JSON/TestMain.java
@@ -1,5 +1,8 @@
 package JSON;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -12,7 +15,7 @@ public class TestMain {
 
 	public static void main(String[] args) throws IOException {
 		
-		ArrayList<LinkProperty> propertyList = new ArrayList<LinkProperty>();
+		/*ArrayList<LinkProperty> propertyList = new ArrayList<LinkProperty>();
 		JsonSocialNetwork jSN= new JsonSocialNetwork();
 		String sNJean = "Jean";
 		String sNJacques = "Jacques";
@@ -25,7 +28,14 @@ public class TestMain {
 		jSN.addLink(jL);
 		jSN.addLink(jL2);
 		
-		String json = JsonConverter.convert(jSN);
+		String json = JsonConverter.convert(jSN);*/
+		
+		/*File file = new File("F:\\PC\\Dowloads\\graphetest.txt"); 
+		BufferedReader br = new BufferedReader(new FileReader(file)); 
+		String json ="";
+		json = br.readLine();
+		br.close();
+		
 		System.out.println(json);
 		JsonSocialNetwork jSN2 = JsonConverter.getJsonSocialNetwork(json);
 		
@@ -57,15 +67,32 @@ public class TestMain {
 			{
 				if(link.getNoeudArrive().getName().equals(node.getName()))
 					node.addLink(link);
-				else if(link.getNoeudArrive().getName().equals(node.getName()))
+				else if(link.getNoeudDepart().getName().equals(node.getName()))
 					node.addLink(link);
 			}
 		}
 		
 		SocialNetwork sNetwork = new SocialNetwork();
+		
 		for(SocialNode node : nodeList)
-			sNetwork.addNode(node);
+			sNetwork.addNode(node);*/
+
+
+		File file = new File("F:\\PC\\Dowloads\\graphetest.txt"); 
+		BufferedReader br = new BufferedReader(new FileReader(file)); 
+		String json ="";
+		json = br.readLine();
+		br.close();
+		
+		SocialNetwork sNetwork = JsonConverter.getSocialNetwork(json);
+		
 		System.out.println(sNetwork.getNbNode());
 		System.out.println(sNetwork.toString());
+		
+		SocialNode sn = sNetwork.getSocialNetwork().get(2);
+		SocialNode sn2 = sNetwork.getSocialNetwork().get(0);
+		
+		System.out.println("\n\n\n"+sn2);
+		System.out.println(sn.getLinkList().get(0).getNoeudDepart());
 	}
 }
