165c9b3a719f218aa720e4e403d8b85b12333089
ValentinTr <valentin54200@gmail.com>
2019-05-22 13:46:03 +0200

    Fixing critical bug link to random

--- src/main/java/reseauSocial/Interface/Main.java
+++ src/main/java/reseauSocial/Interface/Main.java
@@ -3,9 +3,7 @@ package reseauSocial.Interface;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Interface fenetre = new Interface();
-		
+		new Interface();
 	}
 
 }
