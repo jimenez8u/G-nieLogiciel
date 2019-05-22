51af4b0898e92c83dc270f1b37fb9354fc974b0a
MartinLem <martinlemaitre@hotmail.fr>
2019-05-22 09:12:44 +0200

    adding 2 tests for search algos without filters

--- src/test/java/reseauSocial/ParcoursTest.java
+++ src/test/java/reseauSocial/ParcoursTest.java
@@ -2,10 +2,15 @@ package reseauSocial;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.core.JsonParseException;
@@ -18,39 +23,71 @@ import reseauSocial.parcours.Parcours;
 
 class ParcoursTest {
 
-	private final String GRAPHSTRING = "{\"linkList\":["
+	private String graphString = "{\"linkList\":["
 			+ "{\"linkName\":\"l1\",\"properties\":[{\"propertyName\":\"l1\",\"propertyValue\":\"oui\"}],\"noeudDepart\":\"1\",\"noeudArrive\":\"2\"},"
 			+ "{\"linkName\":\"l2\",\"properties\":[{\"propertyName\":\"l2\",\"propertyValue\":\"oui\"}],\"noeudDepart\":\"2\",\"noeudArrive\":\"3\"},"
 			+ "{\"linkName\":\"l3\",\"properties\":[{\"propertyName\":\"l3\",\"propertyValue\":\"oui\"}],\"noeudDepart\":\"3\",\"noeudArrive\":\"4\"},"
 			+ "{\"linkName\":\"l4\",\"properties\":[{\"propertyName\":\"l4\",\"propertyValue\":\"oui\"}],\"noeudDepart\":\"4\",\"noeudArrive\":\"2\"},"
 			+ "{\"linkName\":\"l5\",\"properties\":[{\"propertyName\":\"l5\",\"propertyValue\":\"oui\"}],\"noeudDepart\":\"4\",\"noeudArrive\":\"5\"},"
 			+ "{\"linkName\":\"l6\",\"properties\":[{\"propertyName\":\"l6\",\"propertyValue\":\"oui\"}],\"noeudDepart\":\"1\",\"noeudArrive\":\"5\"}]}";
-	
+
 	private SocialNode s1 = new SocialNode("1");
 	private SocialNode s2 = new SocialNode("2");
 	private SocialNode s3 = new SocialNode("3");
 	private SocialNode s4 = new SocialNode("4");
 	private SocialNode s5 = new SocialNode("5");
-	
+	private SocialNode s8 = new SocialNode("8");
+	private SocialNode s9 = new SocialNode("9");
+
+	private static File file;
+	private static BufferedReader br;
+	private static String graph2;
+
+	@BeforeAll
+	static void initialize() throws IOException {
+		file = new File("files/graphetest.txt");
+		br = new BufferedReader(new FileReader(file));
+		graph2 = br.readLine();
+		br.close();
+	}
+
 	@Test
 	void largeurOrdreTest() throws JsonParseException, JsonMappingException, IOException {
 		List<SocialNode> listResult = Arrays.asList(s1,s2,s5,s3,s4);
 		List<SocialNode> listTest;
-		SocialNetwork sN = JsonConverter.getSocialNetwork(GRAPHSTRING);
-		Parcours p = new Parcours();
+		SocialNetwork sN = JsonConverter.getSocialNetwork(graphString);
 		int index = sN.getSocialNetwork().indexOf(s1);
-		listTest = p.parcoursLargeur(sN.getSocialNetwork().get(index));
+		listTest = Parcours.parcoursLargeur(sN.getSocialNetwork().get(index));
+		assertEquals(listResult, listTest);
+	}
+
+	@Test
+	void largeurOrdreTest2() throws JsonParseException, JsonMappingException, IOException {
+		List<SocialNode> listResult = Arrays.asList(s1, s2, s5, s8, s3, s4, s9);
+		List<SocialNode> listTest;
+		SocialNetwork sN = JsonConverter.getSocialNetwork(graph2);
+		int index = sN.getSocialNetwork().indexOf(s1);
+		listTest = Parcours.parcoursLargeur(sN.getSocialNetwork().get(index));
+		assertEquals(listResult, listTest);
+	}
+
+	@Test
+	void longueurOrdreTest() throws JsonParseException, JsonMappingException, IOException {
+		List<SocialNode> listResult = Arrays.asList(s1, s2, s3, s4, s5);
+		List<SocialNode> listTest;
+		SocialNetwork sN = JsonConverter.getSocialNetwork(graphString);
+		int index = sN.getSocialNetwork().indexOf(s1);
+		listTest = Parcours.parcoursProfondeur(sN.getSocialNetwork().get(index));
 		assertEquals(listResult, listTest);
 	}
 	
 	@Test
-	void longueurOrdreTest() throws JsonParseException, JsonMappingException, IOException  {
-		List<SocialNode> listResult = Arrays.asList(s1,s2,s3,s4,s5);
+	void longueurOrdreTest2() throws JsonParseException, JsonMappingException, IOException {
+		List<SocialNode> listResult = Arrays.asList(s1, s2, s3, s4, s5,s8,s9);
 		List<SocialNode> listTest;
-		SocialNetwork sN = JsonConverter.getSocialNetwork(GRAPHSTRING);
-		Parcours p = new Parcours();
+		SocialNetwork sN = JsonConverter.getSocialNetwork(graph2);
 		int index = sN.getSocialNetwork().indexOf(s1);
-		listTest = p.parcoursProfondeur(sN.getSocialNetwork().get(index));
+		listTest = Parcours.parcoursProfondeur(sN.getSocialNetwork().get(index));
 		assertEquals(listResult, listTest);
 	}
 
